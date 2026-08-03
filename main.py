09074ca2936a290ae2a6b5fc658dd125651a3d32
Tayyiba Rashid <Tayyibarasheed7@gmail.com>
2026-08-03 10:41:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "YAHAN_APNA_TELEGRAM_BOT_TOKEN_LIKHEN"
+TOKEN = "8879657667:AAH5wttxLEZ2OugWdmwgG31NUHkbE8GEv8Y"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot chal raha hai ✅")
